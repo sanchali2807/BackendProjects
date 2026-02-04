dda7288b458f1fa19300fd8942d544975f8cec58
Sanchali <sanchali280705@gmail.com>
2026-02-04 16:31:16 +0530

    backend

---
+++ ecommerce/server.js
@@ -0,0 +1,10 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+const app = express();
+app.use(bodyParser.json());
+app.get("/",(req,res)=>{
+    return res.send("hello world");
+})
+app.listen(8080,()=>{
+    console.log(`App start : http://localhost:8080`);
+});
